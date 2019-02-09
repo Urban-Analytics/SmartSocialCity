3f7401e17a21331ac712a541ebf5dc905c15bc08
Sedar Olmez <solmez@turing.ac.uk>
2019-02-09 19:05:02 +0000

    Bayesian inference project started. Experimenting with probabilistic programming library pyMC3.

---
+++ Probabilistic Programming/Experiments_with_PyMC3/main.py
@@ -0,0 +1,11 @@
+'''
+Name of class: main.py
+Author: Sedar Olmez
+Institute: Leeds Institute for Data Analytics
+Date: 09/02/2019
+
+Class description: This class is a sandbox for executing short methods developed
+from the material in the pymc3 https://docs.pymc.io/ documentation and Bayesian
+Analysis with Python book.
+'''
+import scipy as sp
