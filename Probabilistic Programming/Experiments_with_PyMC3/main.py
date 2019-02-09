9ab1e9b3c707459cdd0ac19d2ce4c73863441041
Sedar Olmez <solmez@turing.ac.uk>
2019-02-09 19:10:50 +0000

    probability distribution.

--- Probabilistic Programming/Experiments_with_PyMC3/main.py
+++ Probabilistic Programming/Experiments_with_PyMC3/main.py
@@ -8,4 +8,8 @@ Class description: This class is a sandbox for executing short methods developed
 from the material in the pymc3 https://docs.pymc.io/ documentation and Bayesian
 Analysis with Python book.
 '''
-import scipy as sp
+from scipy.stats import norm
+
+X = norm(0, 1)
+x = X.rvs(3)
+print(x)
