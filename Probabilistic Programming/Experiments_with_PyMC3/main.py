b3a548ceaa014ed6b8279ea47e85b459d9a937fa
Sedar Olmez <solmez@turing.ac.uk>
2019-02-09 19:37:35 +0000

    The probability distribution graph saves the graphs as a png in the directory.

--- Probabilistic Programming/Experiments_with_PyMC3/main.py
+++ Probabilistic Programming/Experiments_with_PyMC3/main.py
@@ -9,24 +9,8 @@ from the material in the pymc3 https://docs.pymc.io/ documentation and Bayesian
 Analysis with Python book.
 '''
 import probability_functions as pf
-import numpy as np
-import pandas as pd
+
 
 print(pf.probability_distribution_scipy(3))
 
-mu_params = [-1, 0, 1]
-sd_params = [0.5, 1, 1.5]
-x = np.linspace(-7, 7, 200)
-_, ax = plt.subplots(len(mu_params), len(sd_params), sharex = True,
-                    sharey = True, figsize = (9, 7), constrained_layout = True)
-for i in range(3):
-    for j in range(3):
-        mu = mu_params=[i]
-        sd = sd_params[j]
-        y = norm(mu, sd).pdf(x)
-        ax[i, j].plot(x, y)
-        ax[i, j].plot([], label = "u = {:3.2f}\no = {:3.2f}".format(mu, sd), alpha = 0)
-        ax[i, j].legend(loc = 1)
-ax[2, 1].set_xlabel('x')
-ax[1, 0].set_ylabel('p(x)', rotation = 0, labelpad = 20)
-ax[1, 0].set_yticks([])
+pf.probability_distribution_3by3_graphs()
--- Probabilistic Programming/Experiments_with_PyMC3/probability_functions.py
+++ Probabilistic Programming/Experiments_with_PyMC3/probability_functions.py
@@ -8,12 +8,37 @@ Class description: This class is a sandbox for executing short methods developed
 from the material in the pymc3 https://docs.pymc.io/ documentation and Bayesian
 Analysis with Python book.
 '''
+import numpy as np
+import pandas as pd
 from scipy.stats import norm
+from matplotlib import pyplot as plt
+
 
 def __init__():
     pass
+
 
 def probability_distribution_scipy(random_variate_index):
     X = norm(0, 1)
     x = X.rvs(random_variate_index)
     return x
+
+
+def probability_distribution_3by3_graphs():
+    mu_params = [-1, 0, 1]
+    sd_params = [0.5, 1, 1.5]
+    x = np.linspace(-7, 7, 100)
+    f, ax = plt.subplots(len(mu_params), len(sd_params), sharex=True, sharey=True)
+    for i in range(3):
+        for j in range(3):
+            mu = mu_params[i]
+            sd = sd_params[j]
+            y = norm(mu, sd).pdf(x)
+            ax[i,j].plot(x, y)
+            ax[i,j].plot(0, 0,
+            label="$\\mu$ = {:3.2f}\n$\\sigma$ = {:3.2f}".format(mu, sd), alpha=0)
+            ax[i,j].legend(fontsize=12)
+    ax[2,1].set_xlabel('$x$', fontsize=16)
+    ax[1,0].set_ylabel('$pdf(x)$', fontsize=16)
+    plt.tight_layout()
+    plt.savefig('B04958_01_01.png', dpi=300, figsize=(5.5, 5.5))
