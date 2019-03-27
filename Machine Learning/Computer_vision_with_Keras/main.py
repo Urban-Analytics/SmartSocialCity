0a8fa9cff833e9f87ab361a96526478df3a2dbd5
sedar_olmez <solmez@turing.ac.uk>
2019-03-27 22:08:04 +0000

    Added to the code main.py

--- Machine Learning/Computer_vision_with_Keras/main.py
+++ Machine Learning/Computer_vision_with_Keras/main.py
@@ -4,8 +4,8 @@ Date: 17/03/2019
 '''
 
 # Data augmentation
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-
+#from keras.Preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras import *
 datagen = ImageDataGenerator(
     rotation_range = 40,
     width_shift_range = 0.2,
@@ -16,4 +16,13 @@ datagen = ImageDataGenerator(
     fill_mode = 'nearest'
 )
 
-img = load_img('data/train/')
+img = load_img('data/train/cats/cat.0.jpg')
+
+x = img_to_array(img)
+x = x.reshape((1,) + x.shape)
+
+i = 0
+for batch in datagen.flow(x, batch_size = 1, save_to_dir="preview", save_prefix="cat", save_format="jpeg"):
+    i+=1
+    if i > 20:
+        break
